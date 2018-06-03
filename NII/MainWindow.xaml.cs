af9b1f0ccf2ddd531935a48ce414081bece4a130
Viachaslau Donnikov <slavadonnikov@gmail.com>
2018-06-03 23:21:15 +0300

    Header, footer, toggle menu. Date/time.

--- NII/MainWindow.xaml.cs
+++ NII/MainWindow.xaml.cs
@@ -24,5 +24,53 @@ namespace NII   // Программа ведения базы данных "Со
         {
             InitializeComponent();
         }
-    }
+
+		private void Window_Loaded(object sender, RoutedEventArgs e)
+		{
+			GetFullDate();
+			GetRealTime();
+		}
+
+		private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+		{
+			DragMove();
+		}
+
+		private void Application_Shutdown_Click(object sender, RoutedEventArgs e)
+		{
+			Application.Current.Shutdown();
+		}
+
+		#region Application Full Format Date & Time
+		public void GetFullDate()
+		{
+			ApplicationFullDate.Content = DateTime.Now.ToLongDateString();
+			// In ru-RU culture : ApplicationFullDate.Content = DateTime.Now.DayOfWeek.ToString() + ", " + DateTime.Now.ToLongDateString();
+		}
+
+		public void GetRealTime()
+		{
+			System.Timers.Timer Timer = new System.Timers.Timer
+			{
+				Interval = 1000
+			};
+			Timer.Elapsed += Timer_Elapsed;
+			Timer.Start();
+		}
+
+		private void Timer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+		{
+			try
+			{
+				Dispatcher.Invoke(() => ApplicationRealTime.Content = DateTime.Now.ToLongTimeString());
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show("Error : " + ex.Message + ", " + ex.Source);
+			}
+
+			// In ru-RU culture : ApplicationRealTime.Content = DateTime.Now.ToLongTimeString() + " " + DateTime.Now.AddHours(12).ToString("tt", CultureInfo.InvariantCulture)
+		}
+		#endregion
+	}
 }
